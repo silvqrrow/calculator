9b56ebfa7fc9ce18bb1ce1d168979a2434ca3f15
silvqrrow <105092230+silvqrrow@users.noreply.github.com>
2024-05-11 16:55:37 -0400

    Add functions for the basic math operators

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
