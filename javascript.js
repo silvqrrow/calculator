1607632e7d3b5f79184c0c8fd0aee54214a9056e
silvqrrow <105092230+silvqrrow@users.noreply.github.com>
2024-05-12 11:25:31 -0400

    Add operate functoin to be used when the equals sign is pressed

--- javascript.js
+++ javascript.js
@@ -13,3 +13,19 @@ const multiply = (a, b) => {
 const divide = (a, b) => {
   return a / b;
 };
+
+let firstNum;
+let secondNum;
+let operator;
+
+const operate = (firstNum, secondNum, operator) => {
+  if (operator === "+") {
+    return add(firstNum, secondNum);
+  } else if (operator === "-") {
+    return subtract(firstNum, secondNum);
+  } else if (operator === "*") {
+    return multiply(firstNum, secondNum);
+  } else {
+    return divide(firstNum, secondNum);
+  }
+};
